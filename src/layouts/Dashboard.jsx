6129339008c8d2904590d8e9fb0ca577eeb90748
deniz <deniz@DESKTOP-PGJR05G>
2021-07-11 16:24:06 +0300

    ReactCAMP

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
-import { Button, Icon, Label } from 'semantic-ui-react'
+import ProductList from '../pages/ProductList'
+import Categories from './Categories'
+import Navi from './Navi'
 
 export default function Dashboard() {
     return (
         <div>
-            <Button as='div' labelPosition='right'>
-      <Button color='red'>
-        <Icon name='heart' />
-        Like
-      </Button>
-      <Label as='a' basic color='red' pointing='left'>
-        2,048
-      </Label>
-    </Button>
-    <Button as='div' labelPosition='right'>
-      <Button basic color='blue'>
-        <Icon name='fork' />
-        Fork
-      </Button>
-      <Label as='a' basic color='blue' pointing='left'>
-        2,048
-      </Label>
-    </Button>
+            <Navi/>
+            <Categories/>
+            <ProductList/>
+
         </div>
     )
 }
